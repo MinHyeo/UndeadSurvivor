3c0e4a636bef53e84679fc1f3fee3005c676fdf9
MinHyeo <p610222@naver.com>
2024-05-01 20:53:04 +0900

    Study Chapter3
    Undead Survivor 챕터 3까지 공부

---
+++ Undead Survivor/Assets/Undead Survivor/Scripts/Player.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting.InputSystem;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 5f;
+    private Vector2 inputVec;
+
+    private Rigidbody2D rb;
+    private SpriteRenderer sr;
+    private Animator anim;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = inputVec * speed;
+    }
+
+    private void OnMove(InputValue value)
+    {
+        inputVec = value.Get<Vector2>();
+    }
+
+    private void LateUpdate()
+    {
+        anim.SetFloat("Speed", inputVec.magnitude);
+
+        if(inputVec.x != 0){
+            sr.flipX = inputVec.x < 0;
+        }
+    }
+}
